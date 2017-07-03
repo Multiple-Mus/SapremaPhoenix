375ddcaee5008b0799c4dc55b24a69224c531a2a
Seamus O'Higgins <s.o.higgins1986@gmail.com>
2017-07-03 19:37:29 +0100

    Changed consent settings

--- SapremaIdentityServerASP/Config.cs
+++ SapremaIdentityServerASP/Config.cs
@@ -70,7 +70,7 @@ namespace SapremaIdentityServerASP
                     ClientName = "MVC Client",
                     AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
 
-                    RequireConsent = false,
+                    RequireConsent = true,
 
                     ClientSecrets =
                     {
